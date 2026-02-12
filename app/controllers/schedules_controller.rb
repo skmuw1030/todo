da653ed4932b3efa7698657812baf7d94c1ecf3a
ririka nishiuji <rosefur@ririkanoMacBook-Air.local>
2026-02-13 01:36:40 +0900

    new/createを作成（途中）

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,4 +2,32 @@ class SchedulesController < ApplicationController
   def index
     @schedules = Schedule.all
   end
+
+  def new 
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      flash[ :notice ] = "登録しました"
+      redirect_to schedules_path
+    else
+      Rails.logger.debug @schedule.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def schedule_params
+    params.require( :schedule ).permit(
+      :title,
+      :start_date,
+      :end_date,
+      :all_day,
+      :memo
+    )
+  end
+
 end
